54f024ef0d7627834e9e53601270b965bf47ec10
Clément <mail.aatc.c@gmail.com>
2022-11-08 11:55:56 +0100

    maj

--- Création du Dataset/script.py
+++ Création du Dataset/script.py
@@ -10,7 +10,7 @@ from utils import requestSummonerInfo,requestMostRecentGamesIdbis,requestPlayers
 #SCRIPT DE CREATION DU DATASET
 
 #Paramètres
-KEY= "RGAPI-dac1fbc5-756e-4c2e-a5cf-b7a8d1f08bf1"
+KEY= ""
 n_stats = 5 #Nombre de parties sur lesquelles on regarde les stats des joueurs
 size = 5000 #Taille du dataset
 
@@ -97,7 +97,7 @@ def pickle_to_csv(path_pickle,path_csv):
     df = pd.read_pickle(path_pickle)
     df.to_csv(path_csv)
 
-start = 1000 # à modifier: indice de début
-end = 1101 # à modifier: indice de fin (non inclus)
+start = 1101 # à modifier: indice de début
+end = 1201 # à modifier: indice de fin (non inclus)
 main(start,end,n_stats,path=f"Création du Dataset/data/data{start}_{end}.pkl")
 #pickle_to_csv(f"Création du Dataset/data/data{start}_{end}.pkl","Création du Dataset/csv_exemple.csv")
