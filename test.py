1d302028ba827908e69db1640d438c73f677ed12
Clément <103068668+Clemflannagahn@users.noreply.github.com>
2023-02-18 21:22:33 +0100

    maj dataset

---
+++ test.py
@@ -0,0 +1,3 @@
+param_possibles = [(x,y,z,a,b,c) for x in [50] for y in [-80,-90,-100] for z in [60] for a in [-100,-85] for b in [0.15,0.20,0.25,0.30] for c in [3,5,7]]
+
+print(len(param_possibles))
